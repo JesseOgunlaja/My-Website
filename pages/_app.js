f1899d4d9998a823e15c5d0c93e9be30f7e0af7a
Jesse <jesseogunlaja@gmail.com>
2023-04-14 11:20:12 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
-import '@/styles/globals.css'
-import Layout from '@/components/Layout'
-import { Analytics } from '@vercel/analytics/react';
+import "@/styles/globals.css";
+import Layout from "@/components/Layout";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
-      <Analytics/>
+      <Analytics />
     </Layout>
-  )
+  );
 }
