ae49c4742d118d1f37d8119e6e2f858891a00a25
Jesse <jesseogunlaja@gmail.com>
2023-07-29 22:55:13 +0100

    commit

--- pages/api/sendEmail/index.js
+++ pages/api/sendEmail/index.js
@@ -4,8 +4,8 @@ import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
 const redis = new Redis({
-  url: process.env.REDIS_URL || "",
-  token: process.env.REDIS_TOKEN || "",
+  url: process.env.REDIS_URL,
+  token: process.env.REDIS_TOKEN,
 });
 
 const ratelimit = new Ratelimit({
