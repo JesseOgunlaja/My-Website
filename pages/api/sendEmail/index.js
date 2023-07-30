6977367525f6b084e9c221777820902342f118f6
Jesse <jesseogunlaja@gmail.com>
2023-07-30 14:27:40 +0100

    commit

---
+++ pages/api/sendEmail/index.js
@@ -0,0 +1,53 @@
+const nodemailer = require("nodemailer")
+
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+const redis = new Redis({
+  url: process.env.REDIS_URL,
+  token: process.env.REDIS_TOKEN,
+});
+
+const ratelimit = new Ratelimit({
+  redis: redis,
+  limiter: Ratelimit.slidingWindow(1, "1 s"),
+});
+
+export default async function handler(req, res) {
+  if(req.method === "POST") {
+
+    const ip = req.ip ?? "127.0.0.1";
+    const { success } =
+        await ratelimit.limit(ip);
+        
+        if (!success) {
+        return res.status(429).json({ message: `Too many requests from this IP` });
+      }
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      secure: true,
+      auth: {
+        user: "noreply4313@gmail.com",
+        pass: process.env.GMAIL_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: "noreply4313@gmail.com",
+      to: "jesseogunlaja@gmail.com",
+      replyTo: req.body.email,
+      subject: req.body.name,
+      text: req.body.message,
+    };
+
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: "Message sent successfully" });
+  } catch (err) {
+    res.status(400).json({ message: `${err}` });
+  }
+}
+else {
+  res.status(405).json({ message: "Method not allowed" })
+}
+}
