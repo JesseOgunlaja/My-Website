a0a49ea5adbbe9cec90cf89d9d697134834fff08
JesseOgunlaja <jesseogunlaja@gmail.com>
2025-04-04 16:22:11 +0100

    ops: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
@@ -33,7 +34,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className}`}>{children}</body>
+      <body className={`${poppins.className}`}>
+        <Analytics />
+        {children}
+      </body>
     </html>
   );
 }
