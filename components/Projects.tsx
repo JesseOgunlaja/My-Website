41a4cb7114b8c386af07c8d3eec076322d43d4d2
Jesse Ogunlaja <jesseogunlaja@gmail.com>
2026-08-07 14:09:21 +0100

    chore: change link in projects list

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,59 +1,59 @@
+import styles from "@/styles/projects.module.css";
 import Image from "next/image";
 import Link from "next/link";
-import styles from "@/styles/projects.module.css";
 
 export default function Projects() {
-	const multiplier = 260;
-	const width = 2 * multiplier;
-	const height = 1 * multiplier;
+    const multiplier = 260;
+    const width = 2 * multiplier;
+    const height = 1 * multiplier;
 
-	return (
-		<section className={styles.projects}>
-			<h2>My Projects</h2>
-			<div className={styles.projectsList}>
-				<article>
-					<Link href="https://streamthing.dev" target="_blank">
-						<h3>Streamthing</h3>
-						<Image
-							src="/streamthing.png"
-							alt="Streamthing"
-							width={width * 1.8}
-							height={height * 1.8}
-							loading="eager"
-							priority
-							quality={100}
-						/>
-					</Link>
-				</article>
-				<article>
-					<Link href="https://harmoni.jesseogu.dev" target="_blank">
-						<h3>Harmoni</h3>
-						<Image
-							src="/harmoni.png"
-							alt="Harmoni"
-							width={width}
-							height={height}
-							loading="eager"
-							priority
-							quality={100}
-						/>
-					</Link>
-				</article>
-				<article>
-					<Link href="https://shrinkr.dev" target="_blank">
-						<h3>Shrinkr</h3>
-						<Image
-							src="/shrinkr.png"
-							alt="Shrinkr"
-							width={width}
-							height={height}
-							loading="eager"
-							priority
-							quality={100}
-						/>
-					</Link>
-				</article>
-			</div>
-		</section>
-	);
+    return (
+        <section className={styles.projects}>
+            <h2>My Projects</h2>
+            <div className={styles.projectsList}>
+                <article>
+                    <Link href="https://streamthing.dev" target="_blank">
+                        <h3>Streamthing</h3>
+                        <Image
+                            src="/streamthing.png"
+                            alt="Streamthing"
+                            width={width * 1.8}
+                            height={height * 1.8}
+                            loading="eager"
+                            priority
+                            quality={100}
+                        />
+                    </Link>
+                </article>
+                <article>
+                    <Link href="https://harmoni.jesseogu.dev" target="_blank">
+                        <h3>Harmoni</h3>
+                        <Image
+                            src="/harmoni.png"
+                            alt="Harmoni"
+                            width={width}
+                            height={height}
+                            loading="eager"
+                            priority
+                            quality={100}
+                        />
+                    </Link>
+                </article>
+                <article>
+                    <Link href="https://shrinkr.jesseogu.dev" target="_blank">
+                        <h3>Shrinkr</h3>
+                        <Image
+                            src="/shrinkr.png"
+                            alt="Shrinkr"
+                            width={width}
+                            height={height}
+                            loading="eager"
+                            priority
+                            quality={100}
+                        />
+                    </Link>
+                </article>
+            </div>
+        </section>
+    );
 }
